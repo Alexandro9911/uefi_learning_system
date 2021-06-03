9a81d526e066a1a2771531438a366b17d366065b
Лялин Александр <31696942+Alexandro9911@users.noreply.github.com>
2021-06-03 22:38:00 +0300

    bug fix

--- src/Server Scripts/createEmulator.php
+++ src/Server Scripts/createEmulator.php
@@ -1,40 +1,143 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
+function compare_disks($first,$second){
+    $counter = 0;
+    $eq_counting =0;
+    for($i =0; $i < sizeof($first); $i++){
+        if($first[$i] == $second[$i]){
+            $eq_counting++;
+            $counter++;
+        } else {
+            $counter++;
+        }
+    }
+    if($counter == $eq_counting){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 if(isset($_POST['emulator_params']) && isset($_POST['about_ovner'])) {
 
-    $emulator_params_object = json_decode($_POST['emulator_params']);
-    $about_ovner_object = json_decode($_POST['about_ovner']);
-    $emulator = array();
-    $practice = array();
-    $criteries = array();
-    $true_settings = array();
+    $emulator_params_object = json_decode($_POST['emulator_params'],true);
+    $about_ovner_object = json_decode($_POST['about_ovner'],true);
 
-    // создаем образ эмулятора.
-    echo $emulator_params_object['cpu']['system_name'];
+    $connection = mysqli_connect('localhost', 'Alexandro', 'Zxcvb1199nm', 'uefi_proj', '3306');
+    if (!$connection) {
+        echo "connection error";
+        die("Connection failed: " . mysqli_connect_error());
+    } else {
 
-    // создаем практику.
+        $emulator = array();
+        $practice = array();
+        $criteries = array();
+        $true_settings = array();
 
+        // создаем образ эмулятора.
+        $emulator['cpu'] = $emulator_params_object['cpu'];
+        $emulator['motherboard'] = $emulator_params_object['motherboard'];
+        $emulator['listHdd'] = $emulator_params_object['listHdd'];
+        $emulator['listDdr'] = $emulator_params_object['listDdr'];
 
-    // создаем критерии.
+        $all_raid = 0;
+        $counter = 0;
+        for($i = 0; $i < sizeof($emulator_params_object['listHdd']); $i++){
+            $curr = $emulator_params_object['listHdd'][$i];
+            if($curr['raid_req'] = 1 ){
+                $all_raid++;
+                $counter++;
+            }
+            $counter++;
+        }
+        if($all_raid == $counter){
+            $count_eq = 0;
+            for($i = 1; $i < sizeof($emulator_params_object['listHdd']); $i++){
+                $first_hdd = $emulator_params_object['listHdd'][0];
+                $current_hdd = $emulator_params_object['listHdd'][$i];
+                $res_compare =  compare_disks($first_hdd,$current_hdd);
+                if($res_compare == 1){
+                    $count_eq++;
+                }
+            }
+            if($count_eq == $counter){
+                $emulator['ena_raid'] = 1;
+            } else {
+                $emulator['ena_raid'] = 0;
+            }
+        } else {
+            $emulator['ena_raid'] = 0;
+        }
 
-    // создаем правильные настройки эмулятора.
+        $listCores = array();
+        $list_multi = $emulator_params_object['cpu']['multiplayer_by_core'];
+        $arrayMulti = explode('#',$list_multi);
+        for($i = 0; $i < $emulator_params_object['cpu']['quantity_core']; $i++){
+            $str = 'Core #'. $i;
+            $arra = array();
+            $arra['name'] = $str;
+            $arra['multiplayer'] = $arrayMulti[$i];
+            array_push($listCores,$arra);
+        }
 
-//    $connection = mysqli_connect('localhost', 'Alexandro', 'Zxcvb1199nm', 'uefi_proj', '3306');
-//    if (!$connection) {
-//        echo "connection error";
-//        die("Connection failed: " . mysqli_connect_error());
-//    } else {
-//        $sql = "";
-//
-//        $res = mysqli_query($connection, $sql);
-//        if($res){
-//            echo "success";
-//        } else {
-//            echo "error in" . mysqli_error($connection);
-//        }
-//    }
-//    mysqli_close($connection);
+        $emulator['cores'] = $listCores;
+        $listFans = array();
+        $sql_fans = "SELECT id,min_rpm,max_rpm,can_regulate,form_factor,name,cfm  FROM fan_nom where form_factor = 1 AND id=2";
+        $res = mysqli_query($connection, $sql_fans);
+        if($res){
+            $row = mysqli_fetch_array($res,MYSQLI_ASSOC);;
+            array_push($listFans,$row);
+
+            $sql_fans_non_cpu = "SELECT id,min_rpm,max_rpm,can_regulate,form_factor,name,cfm  FROM fan_nom where form_factor = 0 LIMIT 2";
+            $res1 = mysqli_query($connection, $sql_fans_non_cpu);
+            $row1 = mysqli_fetch_array($res1,MYSQLI_ASSOC);
+            array_push($listFans,$row1);
+
+            $emulator['listFan'] = $listFans;
+        } else {
+            echo "error in" . mysqli_error($connection);
+        }
+        $emulator['system_time'] = '00:00';
+        $emulator['system_date'] = '01.01.2000';
+        $emulator['system_lang'] = 'English';
+
+        $params_string = json_encode($emulator);
+        $id_emul = 0;
+        $sql_insert_emulator = "INSERT INTO emulators (params_string) VALUES ('$params_string')";
+        $result = mysqli_query($connection,$sql_insert_emulator);
+        if($result){
+            $sql_id = "SELECT id from emulators WHERE params_string='$params_string'";
+            $result_id = mysqli_query($connection,$sql_id);
+            $row3 = mysqli_fetch_array($result_id,MYSQLI_ASSOC);
+            $id_emul = $row3['id'];
+
+            $my_id= $about_ovner_object['ovner_id'];
+            $for_group = $about_ovner_object['group_id'];
+            $task = $about_ovner_object['task_string'];
+            $theme = $about_ovner_object['theme'];
+            $sql_practice = "INSERT INTO practice (for_group, theme, task, ovner_id, emulator_id)
+            VALUES ('$for_group','$theme','$task','$my_id','$id_emul')";
+            $result = mysqli_query($connection,$sql_practice);
+
+            $sql_get_practice_id = "SELECT id from practice where
+                              for_group='$for_group' AND 
+                              theme='$theme' AND
+                              task='$task' AND
+                              ovner_id='$my_id' AND
+                              emulator_id='$id_emul' ";
+            $result = mysqli_query($connection,$sql_get_practice_id);
+            $result_row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+            $params_string = json_decode($params_string);
+            $id = $result_row['id'];
+            $emulator['practice_id'] = $id;
+            $params_string = json_encode($emulator);
+            echo $params_string;
+        } else {
+            echo 'error in insetrion emulator';
+        }
+    }
+    mysqli_close($connection);
 } else {
     echo "error isset";
 }
--- src/Server Scripts/finallizeCreating.php
+++ src/Server Scripts/finallizeCreating.php
@@ -1,43 +1,45 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-if(isset($_POST['emulator_params']) && isset($_POST['about_ovner'])) {
+if(isset($_POST['emulator_params']) && isset($_POST['about_ovner']) && isset($_POST['practice_id'])) {
 
-    $emulator_params_object = json_decode($_POST['emulator_params'],true);
-    $about_ovner_object = json_decode($_POST['about_ovner'],true);
+    $emulator_params_object = json_decode($_POST['emulator_params']);
+    $about_ovner_object = $_POST['about_ovner'];
 
+    $practice_id = $_POST['practice_id'];
+    $my_id = $_POST['my_id'];
+    $group_id = $_POST['group_id'];
     $connection = mysqli_connect('localhost', 'Alexandro', 'Zxcvb1199nm', 'uefi_proj', '3306');
     if (!$connection) {
         echo "connection error";
         die("Connection failed: " . mysqli_connect_error());
     } else {
 
-        $my_id= $about_ovner_object['ovner_id'];
-        $for_group = $about_ovner_object['group_id'];
-        $task = $about_ovner_object['task_string'];
+        $sql_select_members_of_group ="SELECT user_id from member_list where group_id='$group_id'";
 
-        $sql_practice_id = "SELECT id from practice where 
-                              for_group='$for_group' AND
-                              ovner_id='$my_id' AND
-                              task='$task'";
-        $result = mysqli_query($connection,$sql_practice_id);
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-        $id_practice = $row['id'];
-
-        $members_of_group = array();
-        $members_of_group_sql = "SELECT * from member_list where group_id='$for_group'";
-        $result = mysqli_query($connection,$members_of_group_sql);
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-        for($i = 0; $i < sizeof($row); $i++){
-            $member_id = $row['user_id'];
-            $sql_insert_result = "INSERT into practice_results(user_id, points, practice_id)
-                                values('$member_id','0','$id_practice')";
-            $result = mysqli_query($connection,$sql_insert_result);
+        $res = mysqli_query($connection,$sql_select_members_of_group);
+        $arr = mysqli_fetch_array($res);
+        $c = mysqli_num_rows($res);
+        $params_string = json_encode($emulator_params_object);
+        $counter = 0;
+        for($i = 0; $i < $c; $i++){
+            $user = $arr[$i];
+            $sql = "INSERT into practice_results(user_id, points, practice_id,emulator_string)
+                    values ('$user',0,'$practice_id','$params_string')";
+            $res = mysqli_query($connection,$sql);
+            if(!$res){
+                $counter++;
+                echo 'error insert';
+                break;
+            }
         }
+        if($counter != 0){
+            echo "some error in insertion loop :(";
+        }
+        echo "success";
     }
     mysqli_close($connection);
 } else {
     echo "error isset";
 }
-?>
 
